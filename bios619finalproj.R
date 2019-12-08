024995211311bc1aa930d8fa838720724b2da48d
allenjli <allenjli@umich.edu>
2019-12-08 00:59:40 -0500

    it works!!!! yay

--- bios619finalproj.R
+++ bios619finalproj.R
@@ -29,15 +29,17 @@
 #if all suck (futile) then terminate early
 set.seed(619)
 #ALGORITHM CODE
-STEIN_sim <- function(nsims = 100, npatients, dose_levels, 
+STEIN_sim <- function(nsims, npatients, tox_levels, eff_levels, 
                 phi_0 = 0.3, phi_1 = 0.75*phi_0, phi_2 = 1.25*phi_0,
                 psi_1 = 0.3, psi_2 = 0.8,
-                ncohort = 1, w_tox = 0.5, w_tox ) {
+                ncohort = 1, w_tox = 1, w_eff = 1 ) {
 #loop over simulations
-ndose <- length(dose_levels)
+cohorts <- npatients%/%ncohort+1
+ndose <- length(tox_levels)
 OBD <- rep(0,ndose)
-sim_tox <- rep(0,nsims,ncol = ndose)
-sim_eff <- rep(0,nsims,ncol = ndose)
+sim_tox <- matrix(0,nsims,ncol = ndose)
+sim_eff <- matrix(0,nsims,ncol = ndose)
+sim_level <- matrix(0,nsims,ncol = ndose)
   for(r in 1:nsims) {
 #variable setup
 phi_L = log( (1-phi_1) / (1-phi_0) ) / log( (phi_0*(1-phi_1))/ (phi_1*(1-phi_0)) ) 
@@ -57,9 +59,10 @@ j <- 1 #current dose level
 #   c.) o.w select next dose based on: "exploratory"
 #     j_next = argmax( Pr(qj > psi | yj')) , largest posterior probability in doses
 # in admissible set, Unif(0,1) prior to all qj's (ties go to lower dose)
-for (i in 1:npatients%/%ncohort+1) {
+
+for (i in 1:cohorts) {
   #simulate cohort on dose level
-  np <- min(npatients-ncohort*i,ncohort)
+  np <- min(npatients-ncohort*(i-1),ncohort)
   if (np > 0) {
   ntox[j] <- sim_dose(j,ntox,tox_levels,np)
   neff[j] <- sim_dose(j,neff,eff_levels,np)
@@ -80,18 +83,17 @@ for (i in 1:npatients%/%ncohort+1) {
     if(p_estj <= phi_L) {
       set <- c(j-1,j,j+1) #make sure this is not greater or less than set 
       good_set <- set[set > 0 & set <= ndose]
-      j <- c_doserule() 
-      
+      j <- c_doserule(good_set,neff[good_set],n_lvl[good_set],psi_opt) 
     }
     else {
       set <- c(j-1,j) #same as above
       good_set <- set[set > 0 & set <= ndose]
-      j <- c_doserule()
+      j <- c_doserule(good_set,neff[good_set],n_lvl[good_set],psi_opt) 
     }
     #dunno how to do Pr(q_estj' > psi_opt | yj') yet
   }
   #Safety Rule: Pr(pj > phi0 | xj) > 0.95) [unif(0,1) prior for each pj)]
-  if (calc_dl() > 0.95) {
+  if (calc_dl(ntox[j],n_lvl[j],phi_0) > 0.95) {
     unsafe[j] <- 1
   }
   if(all(unsafe == 1) == TRUE) {
@@ -100,11 +102,11 @@ for (i in 1:npatients%/%ncohort+1) {
   }
  
   #futility: Pr (qj < psi1 | yj ) > 0.98, dose level j eliminated for futility
-  if (calc_dl() > 0.98) {
+  if (calc_dl(neff[j],n_lvl[j],psi_1) < 1-0.98) {
     futile[j] <- 1
   }
   if (all(futile == 1) == TRUE) {
-    print(paste0("Trial stopped early for futility at patient "), i)
+    print(paste0("Trial stopped early for futility at cohort"), i)
     #add params or w/e
     break #stop trial
   }
@@ -113,67 +115,86 @@ for (i in 1:npatients%/%ncohort+1) {
 #step 3: repeat step 2 until max sample size
 #utility function at the end to calculate optimal dose
 #in remark 3, U(pj,qj) = qj - w1pj - w2 pjI(pj > phi_0)
-OBD[r] <- OBD_select(ndose,nlvl,ntox,neff)
+OBD[r] <- OBD_select(ndose,n_lvl,ntox,neff, phi_0, w_tox, w_eff)
 sim_tox[r,] <- ntox
 sim_eff[r,] <- neff
-  }
-#returns a matrix of selected doses, ntox, and idk what else
-info_mat <- matrix(OBD,sim_tox,sim_eff,ncol = ndose)
+sim_level[r,] <- n_lvl
+
+}
+OBD_sims <- rep(0,ndose)
+for (i in 1:ndose) {
+  OBD_sims[i] <- sum(OBD == i)/nsims
+}
+#returns a matrix of OBD selection (%), average ntox, and average neffs
+info_mat <- matrix(c(OBD_sims,colMeans(sim_tox),colMeans(sim_eff),colMeans(sim_level)),ncol = ndose, byrow = TRUE)
+rownames(info_mat) <- c("OBD Selection (%)", "Average Ntox", "Average Neff", "Average Nlvl")
 return(info_mat)
 }
 
 #helper function to sim at the dose level
 sim_dose <- function(j,nevent,est_curv,people) {
   #MOVE TO FUNCTION, eff probs have separate fxn
-  cohort <- rbinom(np,1,tox_levels[j])
+  cohort <- rbinom(people,1,est_curv[j])
   nevent[j] <- nevent[j] + sum(cohort)
-  return(nevent)
+  return(nevent[j])
 }
 
-c_doserule <- function(doseset,neff,nlvl,psi,prior_eff) {
-  if(length(doseset) == 1) {
+c_doserule <- function(doseset,neff,nlvl,psi) {
+  m <- length(doseset) 
+  if(m == 1) {
     return(doseset[1])
   }
   else{ #argmax Pr(qj > psi | yj)
-    calculate_prob()
-    
-    dose_label <- 
+    probs <- rep(0,m)
+    #print(doseset)
+    for (i in 1:m) {
+      probs[i] <- calc_dl(neff[i],nlvl[i],psi)
+      #print(paste("entry:",i, ":" , probs[i] ))
+    }
+    max_dose <- which.max(probs)
+    return(doseset[max_dose])
   }
 }
-
-calc_dl <- function() {
-  
+#unif 0,1 prior default -> beta(alpha = 1, beta = 1)
+#doing beta for now
+calc_dl <- function(nevents,ntotal,prob) { 
+  #posterior is then a+sum xi, beta + n - sum xi
+  posterior <- 1-pbeta(prob,1+ nevents, 1 + ntotal - nevents) #checkcheckechk
 }
 
-OBD_select <- function(j,nlvl,ntox,neff,phi_0) {
+OBD_select <- function(j,nlvl,ntox,neff,phi_0,w1,w2) {
   utility <- rep(0,j)
   tox_est <- est_calc(nlvl,ntox)
   eff_est <- est_calc(nlvl,neff)
   for (i in 1:j) {
-    utility[i] <- eff_est[i] - w1*tox_est[i] - w2*tox_est*indicator(tox_est[i],phi_0)
+    utility[i] <- eff_est[i] - w1*tox_est[i] - w2*tox_est[i]*indicator(tox_est[i],phi_0)
   }
   which.max(utility)
 }
 
 est_calc <- function(nlvl,nevent) {
   nlvl[nlvl == 0] <- 1  
-  nevent/nlvl
+  return(nevent/nlvl)
 }
 indicator <- function(tox,phi) {
   if (tox > phi) {return (1)}
   else { return(0)}
 }
 
-
-
 #actual simulation
 #35 people recruited, cohort size 3
 #target tox 1/3
 
 
-prior_tox <- c(5,10,15,19)/100
-prior_eff_mono <- c(10,20,30,40)/100
-prior_eff_level <- c(10,20,22,25)/100
-prior_eff_quad <- c(12,24,20,15)/100
+prior_tox <- c(5,10,15,19)/100 #given
+prior_eff_mono <- c(20,30,40,50)/100
+prior_eff_level <- c(10,30,40,42)/100
+prior_eff_quad <- c(12,37,50,30)/100
 
 
+set_mono <- STEIN_sim(nsims = 100, npatients = 35, 
+                      tox_levels = prior_tox, eff_levels = prior_eff_mono, 
+                      phi_0 = 0.2,
+                      psi_1 = 0.3, psi_2 = 0.8,
+                      ncohort = 3, w_tox = 1, w_eff = 1 )
+set_mono
